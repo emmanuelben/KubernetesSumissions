168315777e4dc8fd793567735c8ac1b6ee6bdeaa
emmanuelben1999 <emmanuelben1999@gmail.com>
2025-11-26 15:13:12 +0100

    Add todo app with HTML response and environment variable support

--- todo_app/server.js
+++ todo_app/server.js
@@ -3,8 +3,23 @@ const http = require('http');
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from todo app!');
+  if (req.method === 'GET' && req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(`
+      <!DOCTYPE html>
+      <html>
+      <head><title>Todo App</title></head>
+      <body>
+        <h1>Todo Application</h1>
+        <p>Server started in port ${PORT}</p>
+        <p>Welcome to the todo app!</p>
+      </body>
+      </html>
+    `);
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
 });
 
 server.listen(PORT, () => {
